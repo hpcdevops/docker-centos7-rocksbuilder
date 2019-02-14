0d3b2ee702b6ebb3b3fc034f9ecfb8f92c7dd960
HPCDevOPS <hpcdevops@users.noreply.github.com>
2019-02-14 10:17:58 -0800

    Update rollbuild_sequence.sh

--- usr/local/bin/rollbuild_sequence.sh
+++ usr/local/bin/rollbuild_sequence.sh
@@ -20,22 +20,33 @@ fi
 pushd ${ROLL_TO_BUILD}
 
 if [ -f ./bootstrap.sh ] ; then
-    grep -q yum ./bootstrap.sh && sed -i 's/sudo yum/yum/g;s/yum/sudo yum -y/g;' ./bootstrap.sh
+    grep -q yum ./bootstrap.sh && \
+        sed -i 's/sudo yum/yum/g;s/yum/sudo yum -y/g;' ./bootstrap.sh
     chmod +x ./bootstrap.sh
     ./bootstrap.sh
+    sudo make clean && \
+        sudo rm ${PWD}/_arch
 fi
 
 make default
 
-for f in $(find . -name "*.iso"); do isoinfo -R -f -i $f ; done
+for f in $(find . -name "*.iso") ; do
+    echo -e "=-=-=- Roll ISO $f Details =-=-=-\n"
+    ls -l $f
+    echo ""
+    isoinfo -R -f -i $f
+done
 
 popd
 
-echo "=-=-=- Build of ${ROLL_TO_BUILD} required "$(du -s --block-size=1k ${ROLL_TO_BUILD} | awk '{printf "%'\''d KB\n", $1}')" -=-=-="
+echo "=-=-=- Build of ${ROLL_TO_BUILD} required "\
+    $(du -s --block-size=1k ${ROLL_TO_BUILD} | awk '{printf "%'\''d KB\n", $1}')" -=-=-="
 
 pushd ${ROLL_TO_BUILD}
 
-for f in $(find . -name "*.iso"); do mv -v $f ../ ; done
+for f in $(find . -name "*.iso"); do
+    mv -v $f ../
+done
 
 popd
 
